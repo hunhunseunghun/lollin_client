736edd9cea50fe09e5d27513153edf8371f24e43
hunhunseunghun <hunhunseunghun@gmail.com>
2021-05-29 20:59:11 +0900

    modified0529

--- lollin-client/src/component/MainNewChamp/MainNewChamp.jsx
+++ lollin-client/src/component/MainNewChamp/MainNewChamp.jsx
@@ -6,42 +6,69 @@ const server = process.env.REACT_APP_SERVER_URL;
 
 const MainNewChamp = () => {
   const [newChampData, setNewChampData] = useState({
-    champion: {
-      id: null,
-      img: null,
-      skills: [],
-      skillsimg: [],
-      skillwebm: [],
-    },
+    id: "Aatrox",
+    img: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+    skillsimg: [
+      "http://ddragon.leagueoflegends.com/cdn/11.11.1/img/spell/AatroxQ.png",
+      "http://ddragon.leagueoflegends.com/cdn/11.11.1/img/spell/AatroxW.png",
+      "http://ddragon.leagueoflegends.com/cdn/11.11.1/img/spell/AatroxE.png",
+      "http://ddragon.leagueoflegends.com/cdn/11.11.1/img/spell/AatroxR.png",
+      "http://ddragon.leagueoflegends.com/cdn/11.11.1/img/passive/Aatrox_Passive.png",
+    ],
+    skillwebm: [
+      "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_P1.webm",
+      "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_Q1.webm",
+      "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_W1.webm",
+      "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_E1.webm",
+      "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_R1.webm",
+    ],
   });
 
-  useEffect(() => {
-    axios.get(`${server}/champions/updatedchampion`).then((res) => {
-      setNewChampData(res);
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get(`${server}/champions/updatedchampion`).then((res) => {
+  //     setNewChampData(res);
+  //   });
+  // }, []);
 
   return (
     <NewChamp className="newChamp">
-      <img
-        className="ncImg"
-        width="70%"
-        src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Gwen_0.jpg"
-        alt="no images"
-      ></img>
-
-      <SkillsDesc className="ncSkilsDesc">
-        <SkillsImg className="ncSkills">
-          {/* {newChampData.skillsimg.map((ele) => (
-            <img className="ncSkillsImg" src={ele} />
-          ))} */}
-        </SkillsImg>
-
-        <video width="80%" height="80%" muted autoPlay loop>
-          <source src="https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0887/ability_0887_Q1.webm"></source>
-          No Source
-        </video>
-      </SkillsDesc>
+      <section className="searchBoxContainer">
+        <section className="newChampWrapLeft">
+          <div className="videoWrapper">
+            <video muted autoPlay loop>
+              <source src="https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0887/ability_0887_Q1.webm"></source>
+            </video>
+          </div>
+          <div className="optionList">
+            {newChampData.skillsimg.map((ele, idx) => (
+              <span className="optionIcon" key={idx + "skillIcon"}>
+                <span
+                  className="optionIconBorder"
+                  key={idx + "skillIconBorder"}
+                >
+                  <img className="optionButtonImg" src={ele} alt={ele} />
+                </span>
+              </span>
+            ))}
+          </div>
+        </section>
+        <section className="newChampWrapRight">
+          <div className="desc">
+            <div className="descTitle">신규 챔피언</div>
+            <div className="descMain">
+              최근 업데이트 된 신규 챔피언을 미리 체험하고 플레이에 반영해
+              보세요
+            </div>
+          </div>
+          <div className="newChampImgWrap">
+            <img className="newChamImg" src={newChampData.img} />
+            <div className="newChamName">{newChampData.id}</div>
+          </div>
+        </section>
+      </section>
+      <div className="titleWrap">
+        <div className="title">- New champion</div>
+      </div>
     </NewChamp>
   );
 };
--- lollin-client/src/component/MainNewChamp/MainNewChampStyled.jsx
+++ lollin-client/src/component/MainNewChamp/MainNewChampStyled.jsx
@@ -1,19 +1,227 @@
 import styled from "styled-components";
 
 export const NewChamp = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 1fr;
-  height: 35%;
-  background: burlywood;
-  place-items: center;
-`;
+  display: flex;
+  justify-content: center;
+  background-color: black;
+  margin-top: 5rem;
 
-export const SkillsDesc = styled.div`
-  display: grid;
-  grid-template-rows: 1fr 1fr;
-`;
+  .searchBoxContainer {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap-reverse;
+    width: 70%;
+    max-width: 1100px;
+  }
+  // section left
+  .newChampWrapLeft {
+    display: grid;
+    width: 49%;
+    max-width: 550px;
+    min-width: 400px;
+    place-items: center;
+  }
+  .videoWrapper {
+    width: 95%;
+  }
+  video {
+    width: 100%;
+    max-width: 530px;
+    border-radius: 20px;
+  }
 
-export const SkillsImg = styled.img`
-  display: grid
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  .optionList {
+    position: relative;
+    width: 350px;
+    display: inline-flex;
+  }
+
+  .optionIcon {
+    position: relative;
+    display: flex;
+    flex: 1 1;
+    width: 60px;
+    height: 60px;
+  }
+
+  .optionIconBorder {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    transform: translate(10px, 10px) scale(1);
+    transition: transform 1s;
+  }
+  .optionIconBorder :hover {
+    position: absolute;
+    transform: translate(0px, -10px) scale(1);
+    transition: all 1000ms cubic-bezier(0.57, -1, 0.725, -1);
+    cursor: pointer;
+  }
+  .optionButtonImg {
+    width: 100%;
+    height: 100%;
+    border-style: none;
+  }
+  // section right
+  .newChampWrapRight {
+    display: grid;
+    width: 49%;
+    max-width: 550px;
+    min-width: 400px;
+    place-items: center;
+  }
+
+  .desc {
+    width: 90%;
+  }
+  .descTitle {
+    width: 100%;
+    text-align: right;
+    font-size: 2em;
+    color: rgb(255, 196, 0);
+  }
+  .descMain {
+    width: 100%;
+    text-align: right;
+    font-size: 0.7rem;
+    color: white;
+  }
+
+  .newChampImgWrap {
+    position: relative;
+    margin-bottom: 1rem;
+  }
+
+  .newChamImg {
+    width: 98%;
+    max-width: 530px;
+    margin-top: 1rem;
+
+    border-radius: 20px;
+  }
+  .newChamName {
+    position: absolute;
+    right: 10%;
+    bottom: 0;
+    z-index: 10000;
+    font-size: 2rem;
+    color: #ffffff;
+  }
+
+  // section style
+  .titleWrap {
+    width: 1px;
+    background-color: #d69d00cb;
+    margin-left: 3rem;
+  }
+  .title {
+    writing-mode: vertical-lr;
+    font-size: 3px;
+    font-weight: bold;
+    color: #d69d00cb;
+  }
+  @media (min-width: 320px) and (max-width: 479px) {
+    .searchBoxContainer {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap-reverse;
+      width: 300px;
+    }
+    // section left
+    .newChampWrapLeft {
+      display: grid;
+      width: 250px;
+      place-items: center;
+    }
+    .videoWrapper {
+      width: 200px;
+    }
+    video {
+      width: 200px;
+      border-radius: 20px;
+    }
+
+    .optionList {
+      position: relative;
+      width: 230px;
+      display: inline-flex;
+    }
+
+    .optionIcon {
+      position: relative;
+      display: flex;
+      flex: 1 1;
+      width: 30px;
+      height: 30px;
+    }
+
+    .optionIconBorder {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      transform: translate(10px, 10px) scale(1);
+      transition: transform 1s;
+    }
+    .optionIconBorder :hover {
+      position: absolute;
+      transform: scale(0.8);
+      transition: all 1000ms;
+      cursor: pointer;
+    }
+    .optionButtonImg {
+      width: 100%;
+      height: 100%;
+      border-style: none;
+    }
+    // section right
+    .newChampWrapRight {
+      display: grid;
+      width: 50%;
+      max-width: 550px;
+      min-width: 300px;
+      place-items: center;
+    }
+
+    .desc {
+      width: 250px;
+    }
+    .descTitle {
+      width: 100%;
+      text-align: right;
+      font-size: 1rem;
+      color: rgb(255, 196, 0);
+    }
+    .descMain {
+      width: 100%;
+      text-align: right;
+      font-size: 0.6rem;
+      color: white;
+    }
+
+    .newChampImgWrap {
+      position: relative;
+      margin-bottom: 1rem;
+    }
+
+    .newChamImg {
+      width: 200px;
+      max-width: 530px;
+      margin-top: 1rem;
+
+      border-radius: 20px;
+    }
+    .newChamName {
+      position: absolute;
+      right: 10%;
+      bottom: 0;
+      z-index: 10000;
+      font-size: 0.8rem;
+      color: #ffffff;
+    }
+
+    // section style
+    .titleWrap {
+      display: none;
+    }
+  }
 `;
