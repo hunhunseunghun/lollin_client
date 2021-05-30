b092dde3dea3a46922cb122e8c48e325e87c752d
leehojun0314 <dlghwns0314@naver.com>
2021-05-31 02:48:07 +0900

    basic portraits of featured game

--- lollin-client/src/pages/Main/Main.jsx
+++ lollin-client/src/pages/Main/Main.jsx
@@ -1,59 +1,60 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
-import { MainContainer } from "./MainStyled.jsx";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { MainContainer } from './MainStyled.jsx';
 
-import MainTopView from "../../component/MainTopView/MainTopView.jsx";
-import MainChampPre from "../../component/MainChampPre/MainChampPre.jsx";
-import MainNewChamp from "../../component/MainNewChamp/MainNewChamp.jsx";
-import MainUpdatedItems from "../../component/MainUpdatedItems/MainUpdatedItems.jsx";
-import MainSearch from "../../component/MainSearch/MainSearch.jsx";
+import MainTopView from '../../component/MainTopView/MainTopView.jsx';
+import MainChampPre from '../../component/MainChampPre/MainChampPre.jsx';
+import MainNewChamp from '../../component/MainNewChamp/MainNewChamp.jsx';
+import MainUpdatedItems from '../../component/MainUpdatedItems/MainUpdatedItems.jsx';
+import MainSearch from '../../component/MainSearch/MainSearch.jsx';
 
 const server = process.env.REACT_APP_SERVER_URL;
 const MainPage = () => {
-  const [champName, setChampName] = useState("");
-  const [champResult, setChampResult] = useState();
-  const history = useHistory();
+	const [champName, setChampName] = useState('');
+	const [champResult, setChampResult] = useState();
+	const history = useHistory();
 
-  const handleSearch = (champName) => {
-    setChampName(champName);
-  };
+	const handleSearch = (champName) => {
+		setChampName(champName);
+	};
 
-  const handleSearchChange = (event) => {
-    setChampName(event.target.value);
-  };
+	const handleSearchChange = (event) => {
+		setChampName(event.target.value);
+	};
 
-  console.log(champName);
+	console.log(champName);
 
-  const handleSearchClick = () => {
-    axios.get(`${server}/search?name=${champName}`).then((res) => {
-      setChampResult(res.data);
-      console.log(res);
-    });
-    // history.push("/matchinginfo");
-    // history.push("./matchinginfo")
-    //matching info page 로 이동
-  };
+	const handleSearchClick = () => {
+		console.log(server);
+		axios.get(`${server}/search?name=${champName}`).then((res) => {
+			setChampResult(res.data);
+			console.log(res);
+		});
+		// history.push("/matchinginfo");
+		// history.push("./matchinginfo")
+		//matching info page 로 이동
+	};
 
-  // useEffect(() => {
-  //   axios.get(`${server}/utils/rotation`).then((res) => {
-  //     console.log(res);
-  //   });
-  // });
+	// useEffect(() => {
+	//   axios.get(`${server}/utils/rotation`).then((res) => {
+	//     console.log(res);
+	//   });
+	// });
 
-  return (
-    <MainContainer className="mainContainer">
-      <MainTopView />
-      <MainSearch
-        handleSearchChange={handleSearchChange}
-        handleSearchClick={handleSearchClick}
-      ></MainSearch>
+	return (
+		<MainContainer className="mainContainer">
+			<MainTopView />
+			<MainSearch
+				handleSearchChange={handleSearchChange}
+				handleSearchClick={handleSearchClick}
+			></MainSearch>
 
-      <MainChampPre history={history} />
-      <MainNewChamp />
-      <MainUpdatedItems history={history} />
-    </MainContainer>
-  );
+			<MainChampPre history={history} />
+			<MainNewChamp />
+			<MainUpdatedItems history={history} />
+		</MainContainer>
+	);
 };
 
 export default MainPage;
--- lollin-client/src/pages/MatchingInfo/MatchingInfo.jsx
+++ lollin-client/src/pages/MatchingInfo/MatchingInfo.jsx
@@ -1,9 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Div } from './MatchingInfoStyled';
-
+import dotenv from 'dotenv';
+dotenv.config();
 function MatchingInfo() {
-	return <Div>something</Div>;
+	let tempInput = '';
+	let [matchData, setMatchData] = useState({});
+	let [inputName, setInputName] = useState('');
+	let handleSearch = () => {
+		console.log(process.env.REACT_APP_SERVER_URL);
+		axios
+			.get(`https://lollinserver.link/utils/search?name=${inputName}`)
+			.then((response) => {
+				console.log(response.data);
+				console.log(response.status);
+			})
+			.catch((err) => {
+				console.log('this is catch');
+				axios
+					.get(`https://lollinserver.link/utils/featured`)
+					.then((response) => {
+						console.log(response.data);
+						setMatchData(response.data);
+					});
+			});
+	};
+	return (
+		<Div>
+			<input
+				type="text"
+				value={inputName}
+				onChange={(e) => {
+					setInputName(e.target.value);
+				}}
+			></input>
+			<button onClick={handleSearch}>search</button>
+			{matchData.participants
+				? matchData.participants.map((el) => {
+						console.log(el);
+						return (
+							<img
+								src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${el.championName}_0.jpg`}
+								width={80}
+								height={250}
+							></img>
+						);
+				  })
+				: ''}
+		</Div>
+	);
 }
-
+//http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg
 export default MatchingInfo;
