b41d637e339ff5e00ea536da4028b30098971314
leehojun0314 <dlghwns0314@naver.com>
2021-06-01 01:42:30 +0900

    show featured game

--- lollin-client/src/pages/MatchingInfo/MatchingInfo.jsx
+++ lollin-client/src/pages/MatchingInfo/MatchingInfo.jsx
@@ -4,27 +4,48 @@ import { Div } from './MatchingInfoStyled';
 import dotenv from 'dotenv';
 dotenv.config();
 function MatchingInfo() {
-	let tempInput = '';
 	let [matchData, setMatchData] = useState({});
 	let [inputName, setInputName] = useState('');
+	let [isSearched, setIsSearched] = useState(false);
+	let blueTeam = [];
+	let redTeam = [];
+	let setDatas = (matchData) => {
+		blueTeam = [];
+		redTeam = [];
+		setMatchData(matchData);
+		matchData.participants.forEach((participant) => {
+			if (participant.teamId === 100) {
+				blueTeam.push(participant);
+			} else {
+				redTeam.push(participant);
+			}
+		});
+		console.log('redTeam: ', redTeam);
+		console.log('blueTeam: ', blueTeam);
+	};
 	let handleSearch = () => {
-		console.log(process.env.REACT_APP_SERVER_URL);
 		axios
-			.get(`https://lollinserver.link/utils/search?name=${inputName}`)
+			.get(`${process.env.REACT_APP_SERVER_URL}utils/search?name=${inputName}`)
 			.then((response) => {
 				console.log(response.data);
 				console.log(response.status);
+				setIsSearched(true);
+				setDatas(response.data);
 			})
 			.catch((err) => {
 				console.log('this is catch');
 				axios
-					.get(`https://lollinserver.link/utils/featured`)
+					.get(`${process.env.REACT_APP_SERVER_URL}utils/featured`)
 					.then((response) => {
 						console.log(response.data);
-						setMatchData(response.data);
+						setIsSearched(false);
+						setDatas(response.data);
 					});
 			});
 	};
+	useEffect(() => {
+		handleSearch();
+	}, []);
 	return (
 		<Div>
 			<input
@@ -37,16 +58,20 @@ function MatchingInfo() {
 			<button onClick={handleSearch}>search</button>
 			{matchData.participants
 				? matchData.participants.map((el) => {
-						console.log(el);
 						return (
 							<img
 								src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${el.championName}_0.jpg`}
-								width={80}
+								width={120}
 								height={250}
 							></img>
 						);
 				  })
 				: ''}
+			{isSearched ? (
+				<div>The result of {inputName}</div>
+			) : (
+				<div>Showing featured game</div>
+			)}
 		</Div>
 	);
 }
