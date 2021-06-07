857df9e656b3c5ffd59add8686468bdcc9c55832
leehojun0314 <dlghwns0314@naver.com>
2021-06-08 01:25:03 +0900

    bug fix, imagechange

--- lollin-client/src/pages/UserInfo/UserInfo.jsx
+++ lollin-client/src/pages/UserInfo/UserInfo.jsx
@@ -1,31 +1,44 @@
-import React, { useState } from "react";
-import UserInfoMatching from "../../component/UserInfoMatching/UserInfoMatching.jsx";
-import UserInfoHistory from "../../component/UserInfoHistory/UserInfoHistory.jsx";
-import { Container, BackImg, Wrapper, UserTitle, UserContent } from "./UserInfoStyled.jsx";
-import UserInfoBgi from "../../Images/userinfobgi.jpeg";
+import React, { useState } from 'react';
+import UserInfoMatching from '../../component/UserInfoMatching/UserInfoMatching.jsx';
+import UserInfoHistory from '../../component/UserInfoHistory/UserInfoHistory.jsx';
+import {
+	Container,
+	BackImg,
+	Wrapper,
+	UserTitle,
+	UserContent,
+} from './UserInfoStyled.jsx';
+import UserInfoBgi from '../../Images/userinfobgi.jpeg';
 
 const UserInfo = ({ summonerName }) => {
-  let [defaultPlayer, setDefaultPlayer] = useState(null);
+	let [defaultPlayer, setDefaultPlayer] = useState(null);
 
-  return (
-    <Container>
-      <BackImg className="userinfobgi" src={`${UserInfoBgi}`} />
-      <Wrapper>
-        <UserTitle>User Info</UserTitle>
-        <UserContent>
-          <UserInfoMatching
-            summonerName={summonerName}
-            setDefaultPlayer={setDefaultPlayer}
-          />
+	function handlePlayer(player) {
+		if (summonerName.length > 0) {
+			// setDefaultPlayer(summonerName);
+		} else {
+			setDefaultPlayer(player);
+		}
+	}
+	return (
+		<Container>
+			<BackImg className="userinfobgi" src={`${UserInfoBgi}`} />
+			<Wrapper>
+				<UserTitle>User Info</UserTitle>
+				<UserContent>
+					<UserInfoMatching
+						summonerName={summonerName}
+						setDefaultPlayer={handlePlayer}
+					/>
 
-          <UserInfoHistory
-            summonerName={summonerName}
-            defaultPlayer={defaultPlayer}
-          />
-        </UserContent>
-      </Wrapper>
-    </Container>
-  );
+					<UserInfoHistory
+						summonerName={summonerName}
+						defaultPlayer={defaultPlayer}
+					/>
+				</UserContent>
+			</Wrapper>
+		</Container>
+	);
 };
 
 export default UserInfo;
