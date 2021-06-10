a6ae0f8db2be9d076f8237ed8cf888b1a3a2a075
hunhunseunghun <hunhunseunghun@gmail.com>
2021-06-10 21:42:02 +0900

    done:)

--- lollin-client/src/App.js
+++ lollin-client/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Route, useHistory } from "react-router-dom";
 import MainPage from "./pages/Main/Main.jsx";
 import ItemsDB from "./pages/ItemsDB/ItemsDB.jsx";
